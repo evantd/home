babf69ad78dea1e2d26757a5573787601febe7be
Evan Dower <evantd@gmail.com>
2015-08-06 14:56:39 -0700

    Apparently GREP_OPTIONS is deprecated.

--- .zshenv.d/general.zsh
+++ .zshenv.d/general.zsh
@@ -4,4 +4,3 @@ export PATH=$HOME/bin:$PATH
 export PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/kerberos/bin
 
 export LESS="MRiX"
-export GREP_OPTIONS="--color=auto"
