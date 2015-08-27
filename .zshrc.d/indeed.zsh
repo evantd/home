9a8ac00c09048793e47f02bcc830a58ea2da17f1
Evan Dower <edower@indeed.com>
2015-08-27 10:53:41 -0700

    Only init boot2docker when it exists, and init ssh-agent if it isn't already.

--- .zshrc.d/indeed.zsh
+++ .zshrc.d/indeed.zsh
@@ -2,4 +2,11 @@
 if [ -e "$HOME/env/etc/indeedrc" ]; then
     . "$HOME/env/etc/indeedrc"
 fi
-eval "$(boot2docker shellinit)"
+whence boot2docker && eval "$(boot2docker shellinit)"
+if ssh-add -l | grep -q edower
+then
+  # good!
+else
+  eval `ssh-agent`
+  ssh-add
+fi
