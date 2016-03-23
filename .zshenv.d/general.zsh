8b4a926b3f15328bb34b08c1d6a3102a78877221
Evan Dower <edower@indeed.com>
2016-03-23 11:02:43 -0700

    Make 'less' work for gzipped files.

--- .zshenv.d/general.zsh
+++ .zshenv.d/general.zsh
@@ -4,3 +4,5 @@ export PATH=$HOME/bin:$PATH
 export PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/kerberos/bin
 
 export LESS="MRiX"
+
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh /usr/bin/lesspipe)"
