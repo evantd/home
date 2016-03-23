b6eb43b300bfdc30d4ba08f03bf8c1448d54a9a5
Evan Dower <edower@indeed.com>
2016-03-23 11:03:54 -0700

    Skip trying to set SSH_AUTH_SOCK on MacOS.

--- .zshrc.d/100_ssh-agent.zsh
+++ .zshrc.d/100_ssh-agent.zsh
@@ -1,13 +1,19 @@
 # .zshrc.d/prompt vim: set ft=zsh:
 
-function update_ssh_auth_sock () {
-    export SSH_AUTH_SOCK=$(find /tmp/ssh-* -user `whoami` -name agent\* -printf '%T@ %p\n' 2>/dev/null | sort -k 1nr | sed 's/^[^ ]* //' | head -n 1)
-}
-
-typeset -a precmd_functions
-if [ $ZSH_MAJOR_VERSION -gt 4 -o \( $ZSH_MAJOR_VERSION -eq 4 -a $ZSH_MINOR_VERSION -ge 2 \) ]
-then
-    precmd_functions+=update_ssh_auth_sock
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    # No plan for Darwin yet.
 else
-    precmd_functions[$(($#precmd_functions+1))]=update_ssh_auth_sock
+
+    function update_ssh_auth_sock () {
+        export SSH_AUTH_SOCK=$(find /tmp/ssh-* -user `whoami` -name agent\* -printf '%T@ %p\n' 2>/dev/null | sort -k 1nr | sed 's/^[^ ]* //' | head -n 1)
+    }
+
+    typeset -a precmd_functions
+    if [ $ZSH_MAJOR_VERSION -gt 4 -o \( $ZSH_MAJOR_VERSION -eq 4 -a $ZSH_MINOR_VERSION -ge 2 \) ]
+    then
+        precmd_functions+=update_ssh_auth_sock
+    else
+        precmd_functions[$(($#precmd_functions+1))]=update_ssh_auth_sock
+    fi
+
 fi
