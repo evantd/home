4502206db82f8b3a926738d59549c71086924d9a
Evan Dower <edower@indeed.com>
2016-10-10 10:31:26 -0700

    Add 'package' to build alias.

--- .zshrc.d/indeed.zsh
+++ .zshrc.d/indeed.zsh
@@ -2,4 +2,4 @@
 if [ -e "$HOME/env/etc/indeedrc" ]; then
     . "$HOME/env/etc/indeedrc"
 fi
-alias build='ant clean compile test coverage-report </dev/null >&log.txt & ; less +F log.txt'
+alias build='ant clean compile test package </dev/null >&log.txt & ; less +F log.txt'
