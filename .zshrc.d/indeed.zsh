826987bf6e6db7ce06193af44eeed27b5a225611
Evan Dower <edower@indeed.com>
2018-07-27 12:43:14 -0700

    More thorough build alias and add to AUTO_CHECK_REPOS

--- .zshrc.d/indeed.zsh
+++ .zshrc.d/indeed.zsh
@@ -2,4 +2,6 @@
 if [ -e "$HOME/env/etc/indeedrc" ]; then
     . "$HOME/env/etc/indeedrc"
 fi
-alias build='ant clean compile test package </dev/null >&log.txt & ; less +F log.txt'
+alias build='ant clean npm-build npm-test compile test package static </dev/null >&log.txt & ; less +F log.txt'
+export AUTO_CHECK_REPOS=$INDEED_PROJECT_DIR/hobo:$INDEED_PROJECT_DIR/standalone-dradishost:$INDEED_PROJECT_DIR/dradis-hobo-resources:$INDEED_PROJECT_DIR/rhonedelta:$AUTO_CHECK_REPOS
+
