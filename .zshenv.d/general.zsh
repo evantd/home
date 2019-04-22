4ee6e4b09d6a2358feae8f41ad251fb0512a45da
Evan Dower <edower@indeed.com>
2019-04-22 12:54:56 -0500

    Add pyenv things to PATH

--- .zshenv.d/general.zsh
+++ .zshenv.d/general.zsh
@@ -1,7 +1,7 @@
 # .zshenv
 
 export PATH=$HOME/bin:$PATH
-export PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/kerberos/bin
+export PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/kerberos/bin:$HOME/.pyenv/bin
 
 export LESS="MRiX"
 
