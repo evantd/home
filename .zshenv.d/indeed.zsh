c3ff0feef8353fb963c23640f382ed3e40d1ffb5
Evan Dower <edower@indeed.com>
2021-05-25 18:35:11 +0000

    Update Indeed environment variables for system-setup

--- .zshenv.d/indeed.zsh
+++ .zshenv.d/indeed.zsh
@@ -3,10 +3,16 @@ export INDEED_PROJECT_DIR="$HOME/indeed"
 export PATH="$INDEED_PROJECT_DIR/hobo/bin:$PATH"
 export PATH="$PATH:$INDEED_PROJECT_DIR/progex/progex-init/progex-scripts"
 # OPTIONAL, but recommended: Add ~/env/bin to your PATH to use the shared shell scripts from delivery/env
-export PATH="$HOME/env/bin:$PATH"
+if [ -d /opt/indeed/env ]; then
+  export INDEED_ENV_DIR="/opt/indeed/env"
+else
+  export INDEED_ENV_DIR="$HOME/env"
+fi
+export PATH="$INDEED_ENV_DIR/bin:$PATH"
 # indeedrc depends on a different mktemp
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-export PATH="$INDEED_PROJECT_DIR/javadev/ant/bin:$PATH"
+export INDEED_JAVADEV_DIR="$INDEED_PROJECT_DIR/javadev"
+export PATH="$INDEED_JAVADEV_DIR/bin:$PATH"
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 export IVY_IGNORE_RELEASE=0
