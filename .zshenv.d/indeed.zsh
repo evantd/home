435f067e893d4b5237f75908fc1522e0dca1c89c
Evan Dower <edower@indeed.com>
2021-05-27 19:16:20 +0000

    Add system-setup to PATH

--- .zshenv.d/indeed.zsh
+++ .zshenv.d/indeed.zsh
@@ -9,6 +9,7 @@ else
   export INDEED_ENV_DIR="$HOME/env"
 fi
 export PATH="$INDEED_ENV_DIR/bin:$PATH"
+export PATH="$INDEED_PROJECT_DIR/system-setup/bin:$PATH"
 # indeedrc depends on a different mktemp
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export INDEED_JAVADEV_DIR="$INDEED_PROJECT_DIR/javadev"
