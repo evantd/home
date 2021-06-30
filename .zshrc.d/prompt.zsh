0e557bc8ecd155d770ab022428805ab90e8e5cd0
Evan Dower <edower@indeed.com>
2021-06-30 14:49:23 -0700

    Update prompt in a different way

--- .zshrc.d/prompt.zsh
+++ .zshrc.d/prompt.zsh
@@ -5,7 +5,7 @@ function getZshrcGitCommit {
 }
 export ZSHRC_GIT_COMMIT=`getZshrcGitCommit`
 
-autoload colors
+autoload -U colors
 colors
 typeset -A pp # _P_rompt _P_arts
 pp[end]="$bg[default]$fg_no_bold[default]"
@@ -18,20 +18,15 @@ then
 else
     pp[user]="$bg[default]$fg_bold[red]"
 fi
-if [ "x`hostname`" = "xedower.desktop.amazon.com" ]
-then
-    pp[host]="$bg[default]$fg_bold[green]"
-else
-    pp[host]="$bg[default]$fg_bold[red]"
-fi
+pp[host]="$bg[default]$fg_bold[green]"
 pp[path]="$bg[default]$fg_no_bold[default]"
 
-function update_prompt_colors {
+function update_prompt {
     # make nasty colors when I don't have a valid kerberos ticket or my home repo is out-of-date
 
     if [ "$KRB_IN_USE" = "yes" ]
     then
-        if klist -5cs
+        if klist -5 --test
         then
             auth_up_to_date="yes"
         else
@@ -57,31 +52,21 @@ function update_prompt_colors {
     else
         pp[punct]="$bg[red]$fg_bold[default]"
     fi
+    PS1="%{${pp[punct]}%}[" # [
+    PS1+="%{${pp[user]}%}%n" # user
+    PS1+="%{${pp[punct]}%}@" # @
+    PS1+="%{${pp[host]}%}%m" # host
+    PS1+="%{${pp[punct]}%}:" # :
+    PS1+="%{${pp[path]}%}%1~" # path
+    PS1+="%{${pp[punct]}%}]" # ]
+    PS1+="%{${pp[level]}%}%L" # $SHLVL
+    PS1+="%{${pp[punct]}%}^" # ^
+    PS1+="%{${pp[jobs]}%}%j" # number of jobs
+    PS1+="%{${pp[punct]}%}%#" # % for normal users, # for priveleged users
+    PS1+="%{${pp[end]}%} " # return formatting to normal and end prompt with a space
 }
 typeset -a precmd_functions
-if [ $ZSH_MAJOR_VERSION -gt 4 -o \( $ZSH_MAJOR_VERSION -eq 4 -a $ZSH_MINOR_VERSION -ge 2 \) ]
-then
-    precmd_functions+=update_prompt_colors
-else
-    precmd_functions[$(($#precmd_functions+1))]=update_prompt_colors
-fi
+precmd_functions+=update_prompt
 
-setopt prompt_subst # expand variables (etc.) in prompt at display time
-if [ $ZSH_MAJOR_VERSION -gt 4 -o \( $ZSH_MAJOR_VERSION -eq 4 -a $ZSH_MINOR_VERSION -ge 2 \) ]
-then
-    PS1='%{${pp[punct]}%}[' # [
-    PS1+='%{${pp[user]}%}%n' # user
-    PS1+='%{${pp[punct]}%}@' # @
-    PS1+='%{${pp[host]}%}%m' # host
-    PS1+='%{${pp[punct]}%}:' # :
-    PS1+='%{${pp[path]}%}%1~' # path
-    PS1+='%{${pp[punct]}%}]' # ]
-    PS1+='%{${pp[level]}%}%L' # $SHLVL
-    PS1+='%{${pp[punct]}%}^' # ^
-    PS1+='%{${pp[jobs]}%}%j' # number of jobs
-    PS1+='%{${pp[punct]}%}%#' # % for normal users, # for priveleged users
-    PS1+='%{${pp[end]}%} ' # return formatting to normal and end prompt with a space
-else
-    PS1='%{${pp[punct]}%}[%{${pp[user]}%}%n%{${pp[punct]}%}@%{${pp[host]}%}%m%{${pp[punct]}%}:%{${pp[path]}%}%1~%{${pp[punct]}%}]%{${pp[level]}%}%L%{${pp[punct]}%}^%{${pp[jobs]}%}%j%{${pp[punct]}%}%#%{${pp[end]}%} '
-fi
+update_prompt
 export PS1
