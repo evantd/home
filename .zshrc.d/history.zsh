33132288a363247f67ed4a7176bc0b8eb95be3f8
Evan Dower <edower@indeed.com>
2021-07-27 17:06:50 +0000

    Simplify & improve zsh history settings

--- .zshrc.d/history.zsh
+++ .zshrc.d/history.zsh
@@ -1,5 +1,5 @@
 # .zshrc.d/history.zsh # vim: set ft=zsh:
 
-setopt incappendhistory sharehistory extendedhistory histreduceblanks
+setopt SHARE_HISTORY HIST_REDUCE_BLANKS HIST_IGNORE_DUPS
 export HISTSIZE=10000 SAVEHIST=10000 HISTFILE=~/.history
 export REPORTTIME=10
