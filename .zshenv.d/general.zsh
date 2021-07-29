b6c0181e58ea2ab1d0bacac2621d83362e2e77ab
Evan Dower <edower@indeed.com>
2021-07-29 13:38:00 -0700

    Set COLORTERM if TERM=alacritty

--- .zshenv.d/general.zsh
+++ .zshenv.d/general.zsh
@@ -6,3 +6,8 @@ export PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin
 export LESS="MRiX"
 
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh /usr/bin/lesspipe)"
+
+if [ "x$TERM" = "xalacritty" ]
+then
+    COLORTERM="truecolor"
+fi
