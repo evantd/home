07b9f55dab0a4e9aa5b4fe54c997c4d63f05482a
Evan Dower <edower@indeed.com>
2025-02-07 00:15:02 +0000

    Configure the SourceGraph CLI

--- .zshenv.d/indeed.zsh
+++ .zshenv.d/indeed.zsh
@@ -3,3 +3,9 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 # Save disk space by discarding previous versions of artifacts.
 export PREVIOUS_ITEMS_TO_KEEP=0
 export MAX_KEPT=1
+
+# SourceGraph CLI setup
+# https://sourcegraph.com/docs/cli/quickstart
+export SRC_ENDPOINT=https://indeed.sourcegraph.com
+# See sourcegraph_token.zsh for this next bit, and don't commit it.
+#SRC_ACCESS_TOKEN=sgp_dontstealmytokenbro
